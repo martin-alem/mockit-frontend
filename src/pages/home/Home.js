c9c8e39bf6645364b3676dc9e16dfbc7d710bfdc
Martin Alemajoh <alemajohmartin@gmail.com>
2021-11-14 10:39:08 -0500

    implemented the feature section

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,16 @@
 import React from "react";
 import Header from "./../../components/header/Header";
 import Intro from "./../../components/intro/Intro";
-import Container from "@mui/material/Container"
+import Container from "@mui/material/Container";
+import Features from "./../../components/features/Features";
 
 function Home() {
   return (
     <>
       <Header />
-      <Container maxWidth="md" sx={{mt: "50px"}}>
+      <Container maxWidth="md" sx={{ mt: "50px" }}>
         <Intro />
+        <Features />
       </Container>
     </>
   );
