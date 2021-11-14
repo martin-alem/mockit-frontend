669907e4840c4d91ce4221b444184bf9cc62ee2d
Martin Alemajoh <alemajohmartin@gmail.com>
2021-11-14 08:53:01 -0500

    add test for Heading component

--- src/components/heading/Heading.js
+++ src/components/heading/Heading.js
@@ -7,12 +7,13 @@ function Heading() {
     <Box
       sx={{
         width: { xs: "100%", md: "50%" },
+        color: "primary.contrastText",
       }}
     >
-      <Typography variant="h1" component="div" color="primary.contrastText" gutterBottom>
+      <Typography variant="h1" data-testid="h1" component="h1" gutterBottom>
         Mockit
       </Typography>
-      <Typography variant="subtitle1" color="primary.contrastText" gutterBottom component="div">
+      <Typography variant="subtitle1" data-testid="h4" gutterBottom component="h4">
         The ultimate platform to practice mock coding interviews. Everything you need to get comfortable tackling any software technical and behavioral interview.
       </Typography>
     </Box>
