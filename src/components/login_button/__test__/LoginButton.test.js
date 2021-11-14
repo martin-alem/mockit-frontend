3b8d8d288e9861cf1edff73d016ce4ae7f6c8920
Martin Alemajoh <alemajohmartin@gmail.com>
2021-11-14 08:28:28 -0500

    add test for LoginButton component

---
+++ src/components/login_button/__test__/LoginButton.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import LoginButton from "./../LoginButton";
+
+let button = null;
+
+beforeEach(() => {
+  render(<LoginButton />);
+  button = screen.getByRole("button");
+});
+
+test("should render the Button component and should be in document", () => {
+  expect(button).toBeInTheDocument();
+});
+
+test("should render the Button component and should be visible", () => {
+  expect(button).toBeVisible();
+});
