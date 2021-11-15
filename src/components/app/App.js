d31fabdf138e240c6ae75db9454963d1ca4c0542
Martin Alemajoh <alemajohmartin@gmail.com>
2021-11-14 20:28:47 -0500

    added react-router-dom

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,11 +2,16 @@ import React from "react";
 import mainTheme from "./../theme/mainTheme";
 import { ThemeProvider } from "@mui/material";
 import Home from "./../../pages/home/Home";
+import Profile from "./../../pages/profile/Profile";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={mainTheme}>
-        <Home />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/profile" exact component={Profile} />
+      </Switch>
     </ThemeProvider>
   );
 }
